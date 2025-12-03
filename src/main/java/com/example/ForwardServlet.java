681209f54973c594a7a8b37b514e41f870cbf4d6
miku <2068367594@qq.com>
2025-12-03 09:49:36 +0800

    feat: add cookie

--- src/main/java/com/example/ForwardServlet.java
+++ src/main/java/com/example/ForwardServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 /**
@@ -19,9 +20,15 @@ public class ForwardServlet extends HttpServlet {
         // 设置请求编码为UTF-8
         request.setCharacterEncoding("UTF-8");
         Cookie[] cookie = request.getCookies();
-        // 使用sendRedirect重定向到HelloServlet
-        // 注意：sendRedirect会在客户端发起新的请求，原请求中的属性会丢失
-        response.sendRedirect("/hello");
+        // 如果请求中没有cookie，则在响应中设置一个cookie
+        boolean hasCookie = (cookie != null && cookie.length > 0);
+        if (!hasCookie) {
+            Cookie newCookie = new Cookie("forwardCookie", String.valueOf(System.currentTimeMillis()));
+            newCookie.setPath("/");
+            newCookie.setMaxAge(60 * 60 * 24); // 1天
+            response.addCookie(newCookie);
+        }
+
     }
 
     @Override
@@ -30,8 +37,14 @@ public class ForwardServlet extends HttpServlet {
         // 设置请求编码为UTF-8
         request.setCharacterEncoding("UTF-8");
 
-        // 使用sendRedirect重定向到HelloServlet
-        // 注意：sendRedirect会在客户端发起新的请求，POST请求会变成GET请求
-        response.sendRedirect("/hello");
+        Cookie[] cookie = request.getCookies();
+        // 如果请求中没有cookie，则在响应中设置一个cookie
+        boolean hasCookie = (cookie != null && cookie.length > 0);
+        if (!hasCookie) {
+            Cookie newCookie = new Cookie("forwardCookie", String.valueOf(System.currentTimeMillis()));
+            newCookie.setPath("/");
+            newCookie.setMaxAge(60 * 60 * 24); // 1天
+            response.addCookie(newCookie);
+        }
     }
 }
